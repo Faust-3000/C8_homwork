357392ad0293074264bfd99ad5e4578b40984b3b
Faust3000 <mephistopheles3000@gmail.com>
2022-11-14 20:50:53 +0400

    задача 2, готова

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@
 9 5 3 2
 8 4 4 2 */
 
-Console.WriteLine("Введите количество строк: ");
+/*Console.WriteLine("Введите количество строк: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите количество столбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
@@ -67,10 +67,75 @@ void SortDescendingArray(int[,] matr)
         }
     }
 }
+*/
+
+
+/*Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с 
+наименьшей суммой элементов: 1 строка */
+
+/*Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+Console.WriteLine("Заданный массив: ");
+
+FillArray(array);
+PrintArray(array);
+MinSum(array);
 
 
 
 
 
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 21);
+        }
+    }
+}
 
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " " );
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
+void MinSum(int[,] matr)
+{
+    int count = 0;
+    int minsum = Int32.MaxValue;
+    for (int i = 0; i < rows; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < columns; j++)
+        {
+            sum += matr[i, j];
+        }
+        if (sum < minsum)
+        {
+            minsum = sum;
+            count=i;
+        }
+    }
+    Console.WriteLine("Cтрока номер " + (count+1) + " с наименьшей суммой элементов, равной: " + (minsum));
+}
+*/
