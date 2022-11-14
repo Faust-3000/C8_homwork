e0d59d964d4377ae514b5da997782b80d591ff52
Faust3000 <mephistopheles3000@gmail.com>
2022-11-14 20:35:05 +0400

    задача 1 готова

--- Program.cs
+++ Program.cs
@@ -1,1 +1,76 @@
-﻿
+﻿/*Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию
+элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+Console.WriteLine("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, columns];
+
+Console.WriteLine("Заданный массив: ");
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine("Отсортированный массив: ");
+
+SortDescendingArray(array);
+PrintArray(array);
+
+
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(0, 21);
+        }
+    }
+}
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " " );
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void SortDescendingArray(int[,] matr)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            for (int k = 0; k < columns - 1; k++)
+                if (matr[i, k] < matr[i, k + 1])
+                {
+                    int temp = matr[i, k + 1];
+                    matr[i, k + 1] = matr[i, k];
+                    matr[i, k] = temp;
+                }
+        }
+    }
+}
+
+
+
+
+
+
+
